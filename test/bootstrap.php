f8cfc66d5d08b21f1ef459f372aab50ec9884244
Kevin <kbeau@regard.com>
2014-05-12 12:20:56 -0700

    missing 5.2.x compatibility in tests

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,4 +1,5 @@
 <?php
+  if ( !defined('__DIR__') ) define('__DIR__', dirname(__FILE__)); //5.2.x compatibility
   if (!file_exists(__DIR__.'/unit/config.php'))
   {
     copy(__DIR__.'/unit/config_test.php',__DIR__.'/unit/config.php');
